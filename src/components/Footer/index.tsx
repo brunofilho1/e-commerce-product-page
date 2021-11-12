8642137e0b3a742ef37c7650fe21f1a180537757
Bruno Oliveira <brunowalker551@gmail.com>
2021-11-12 20:11:12 -0300

    created footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,45 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { 
+  FooterDiv,
+  Container, 
+  Button,
+  Div,
+  DivButton,
+  Icons,
+  FacebookIcon,
+  InstagramIcon,
+  TwitterIcon,
+  GithubIcon,
+} from "./styles";
 
 const Footer: React.FC = () => {
-  return <Container>Interface reproduzida com a Rocketseat</Container>;
+  return (
+    <Container>
+      <FooterDiv>
+        <Div className="logo">
+          <h1>CompanyLogo</h1>
+        </Div>
+        <Div>
+          <Icons>
+            <FacebookIcon className="icon" />
+            <InstagramIcon className="icon" />
+            <TwitterIcon className="icon" />
+            <GithubIcon className="icon" />
+          </Icons>
+        </Div>
+        <Div>
+          <h2>Contato</h2>
+          <p>example@gmail.com</p>
+          <p>(38) 98824-2257</p>
+        </Div>
+        <DivButton className="signIn">
+          <Button solid>Entrar</Button>
+          <Button>Registrar-se</Button>
+        </DivButton>
+      </FooterDiv>
+    </Container>
+  )
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,14 +1,81 @@
 import styled from "styled-components";
 
+import { AiFillFacebook, AiFillInstagram, AiOutlineTwitter, AiFillGithub } from 'react-icons/ai'
+
 export const Container = styled.div`
   width: 100%;
-  height: 48px;
-  background: var(--color-footer);
-  border-top: 1px solid var(--color-border);
+  height: 30vh;
+  background: var(--color-header);
+
+  border-bottom: 1px solid var(--color-border);
+  color: white;
 
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
+`;
 
-  margin-top: 30px;
+export const FooterDiv = styled.div`
+  width: 100%;
+  height: 80px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+export const DivButton = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+export const Div = styled.div`
+
+`;
+
+type ButtonProps = {solid?: boolean}
+
+export const Button = styled.button<ButtonProps>`
+  display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 15px;
+    border-radius: 4px;
+    padding: 12px 10px;
+    margin-top: 10px;
+
+    color: ${props => props.solid ? 'var(--color-white)' : 'var(--color-blue)'};
+    background: ${props => props.solid ? 'var(--color-blue)' : 'var(--color-white)'};
+    border: none;
+
+    cursor: pointer;
+
+    &:hover {
+        filter: brightness(1.1);
+    }
+`;
+
+export const Icons = styled.div`
+  .icon {
+    width: 42px;
+    height: 28px;
+    cursor: pointer;
+
+    &:hover {
+        padding: 1px;
+    }
+  }
+`;
+
+export const FacebookIcon = styled(AiFillFacebook)`
+
+`;
+export const InstagramIcon = styled(AiFillInstagram)`
+    
+`;
+export const TwitterIcon = styled(AiOutlineTwitter)`
+    
+`;
+export const GithubIcon = styled(AiFillGithub)`
+    
 `;
